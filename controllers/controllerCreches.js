30aabb2ea994d04cb4c50fc642dba39a0c601e9a
Daniel Dalpasquale <danieldalpasquale@hotmail.com>
2022-05-30 22:36:46 -0300

    .

--- controllers/controllerCreches.js
+++ controllers/controllerCreches.js
@@ -2,7 +2,7 @@ const Creche = require('../models/models_nosql/creches');
 
 module.exports = {
     async getCreate(req, res) {
-        res.render('creche/crecheCreate');
+        res.render('/view/creche/crecheCreate.');
     },
     async postCreate(req, res) {
         const {nome, endereco, numero} = req.body;
@@ -14,12 +14,12 @@ module.exports = {
     },
     async getList(req, res) {
         Creche.find().then((creche) => {
-            res.render('creche/crecheList', {creches: creches.map(creches => creches.toJSON())});
+            res.render('/view/crecheList', {creches: creches.map(creches => creches.toJSON())});
         });
     },
     async getEdit(req, res) {
         await Creche.findOne({ _id: req.params.id }).then((creches) => {
-            res.render('creche/crecheEdit', { creches: creches.toJSON() });
+            res.render('/view/creche/crecheEdit', { creches: creches.toJSON() });
         });
     },
     async postEdit(req, res) {
@@ -27,10 +27,10 @@ module.exports = {
         const imagem = req.imageName;
         console.log(imagem);
         await Creche.findOneAndUpdate({_id:req.body.id}, {nome, endereco, numero,imagem});
-        res.redirect('/crecheList');
+        res.redirect('/view/crecheList');
     },
     async getDelete(req, res) {
         await Creche.findOneAndRemove({ _id: req.params.id });
-        res.redirect('/crecheList');
+        res.redirect('/view/crecheList');
     }
 }
--- routers/route.js
+++ routers/route.js
@@ -1,19 +1,47 @@
 const express = require('express');
-const controller = require('../controllers/controller');
+const controller = require('../controllers/controllerUsuario');
+const controller = require('../controllers/controllerCreches');
+const multer = require('multer');
 const route = express.Router();
 
 module.exports = route;
 
-//home
-route.get("/home",controller.getHome);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/uploads/")
+    },
+    filename: (req, file, cb) => {
+      req.imageName = req.body.nome+'.png'
+      cb(null,  req.imageName)
+    },
+  })
+const upload = multer({storage})
 
+route.get("/home",function(req,res){
+    res.render('home');
+});
+route.get("/logout", controllerUsuario.getLogout);
 
-//login e recuperação de senha
-route.get("/",controller.getLogin);
-route.post("/login",controller.postLogin);
-route.get("/recuperarSenha:login",controller.getRecuperarSenha);
+//Controller Usuario
+//Usuario - Login e Recuperação de Senha
+route.get("/", controllerUsuario.getLogin);
+route.post("/login", controllerUsuario.postLogin);
+route.get("/recuperarSenha/:login", controllerUsuario.getRecuperarSenha);
+route.post("/recuperarSenha", controllerUsuario.postRecuperarSenha);
+//Usuario - CRUD
+route.get("/usuarioCreateADM", controllerUsuario.getCreate);
+route.post("/usuarioCreateADM", controllerUsuario.postCreate);
+route.get("/usuarioCreateDiretor", controllerUsuario.getCreate);
+route.post("/usuarioCreatDiretor", controllerUsuario.postCreate);
+route.get("/usuarioCreatePais", controllerUsuario.getCreate);
+route.post("/usuarioCreatePais", controllerUsuario.postCreate);
+route.get("/usuarioList", controllerUsuario.getList);
 
-//crud Usuario
-route.get("/usuarioCreateADM",controller.getUsuarioCreateADM);
-route.post("/usuarioCreateADm",controller.postUsuarioCreateADM);
-route.get("/usuarioList",controller.getListUsuario);
+//Controller Receita
+//Receita-CRUD
+route.get("/crecheCreate", controllerReceita.getCreate);
+route.post("/crecheCreate",upload.single('imagem'),controllerReceita.postCreate);
+route.get("/crecheList", controllerReceita.getList);
+route.get("/crecheEdit/:id", controllerReceita.getEdit);
+route.post("/crecheEdit",upload.single('imagem'),controllerReceita.postEdit);
+route.get("/crecheDelete/:id", controllerReceita.getDelete);
